1368380b014d5fa191013ac8dc500cf1c0d46397
escojwd <junior19941703@gmail.com>
2021-05-30 18:07:28 +0400

    functionality

--- react-calculator/src/App.js
+++ react-calculator/src/App.js
@@ -4,59 +4,93 @@ import NumberBtn from './Components/Number-Btn/NumberBtn';
 import './App.css';
 
 function App() {
-  function whatsThat(e) {
-    console.log(e.target.textContent);
-  }
-
-  const [sum, setSum] = useState(0);
-
-  const [oldSum, setOldSum] = useState(sum);//form mathAct
-  // const [oldSum, setOldSum] = useState(sum);//form mathAct
+  const [calc, setCalc] = useState(0);
+  const [newNumber, setNewNumber] = useState(0);//for mathAct
+  const [oldNumber, setOldNumber] = useState(0);//for mathAct
+  const [act, setAct] = useState('');
+  const [newCalc, setNewCalc] = useState(true);
+  // const [] = useState();
 
 
   function newSum(e) {
+    if (!newCalc) {
+      setNewCalc(true);
+      clearSum();
+      setCalc(e.target.textContent);
+      // renderCalc(e)
+      return
+    }
+    renderCalc(e)
+  }
+
+  function renderCalc(e) {
     if (e.target.textContent === '0') {
-      if (sum === 0) {
-        setSum(0);
-        setOldSum(sum);
-        console.log(oldSum);
-        console.log(sum);
+      if (calc === 0) {
+        setCalc(0);
+        setNewNumber(0);
         return
       }
-      setSum(prev => prev + e.target.textContent);
-      setOldSum(sum);
+      setCalc(prev => prev + e.target.textContent);
+      setNewNumber(calc);
     }
     if (e.target.textContent !== '0') {
-      if (sum === 0) {
-        setSum(e.target.textContent);
-        setOldSum(sum);
-        console.log(oldSum);
-        console.log(sum);
+      if (calc === 0) {
+        setCalc(e.target.textContent);
+        setNewNumber(calc)
         return;
-        // console.log(e.target.textContent);
       }
-      setSum(prev => prev + e.target.textContent);
-      setOldSum(sum);
-      console.log(oldSum);
-      console.log(sum);
+      setCalc(prev => prev + e.target.textContent);
+      setNewNumber(prev => prev + e.target.textContent);
     }
   }
 
   function clearSum() {
-    setSum(0);
+    setCalc(0);
+    setNewNumber(0);
+    setOldNumber(0);
+    setNewCalc(true);
   }
 
   function mathOpe(e) {
-    if (e.target.textContent === '+') {
-      setOldSum(sum);
-      console.log(oldSum);
-      console.log(sum);
+    // if (act === e.target.textContent) {
+    //   return;
+    // } else {
+    // setCalc(calc.substr(0, calc.length - 1) + e.target.textContent);
+    setOldNumber(calc);
+    setCalc(prev => prev + e.target.textContent);
+    setNewNumber(0);
+    setAct(e.target.textContent);
+    setNewCalc(true)
+    // }
+  }
+
+  function equalSum() {
+    setNewCalc(false);
+
+    if (act === '+') {
+      setCalc(parseInt(oldNumber) + parseInt(newNumber));
+      return
+    }
+    if (act === '-') {
+      setCalc(parseInt(oldNumber) - parseInt(newNumber));
+      return
+
+    }
+    if (act === '*') {
+      setCalc(parseInt(oldNumber) * parseInt(newNumber));
+      return
+    }
+    if (act === '/') {
+      setCalc(parseInt(oldNumber) / parseInt(newNumber));
+      return
     }
   }
 
+  // console.log(Number('23'));
+
   return (
     <div className="App">
-      <p>{sum}</p>
+      <p>{calc}</p>
       <div className='numbs-wrap'>
         <button onClick={clearSum} className='clear-btn'>Clear</button>
         <NumberBtn numb='0' action={newSum} />
@@ -71,11 +105,11 @@ function App() {
         <NumberBtn numb='9' action={newSum} />
       </div>
       <div className='actions-wrap'>
-        <ActionBtn mathAct='=' action={whatsThat} />
+        <ActionBtn mathAct='=' action={equalSum} />
         <ActionBtn mathAct='+' action={mathOpe} />
-        <ActionBtn mathAct='-' action={whatsThat} />
-        <ActionBtn mathAct='*' action={whatsThat} />
-        <ActionBtn mathAct='/' action={whatsThat} />
+        <ActionBtn mathAct='-' action={mathOpe} />
+        <ActionBtn mathAct='*' action={mathOpe} />
+        <ActionBtn mathAct='/' action={mathOpe} />
       </div>
     </div>
   );
